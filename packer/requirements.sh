f0122ef7b0bf6a8bba4edda66673139977f817a2
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-04-07 17:14:59 +0200

    don't clone repo

--- packer/requirements.sh
+++ packer/requirements.sh
@@ -1,8 +1,5 @@
 #/bin/bash -e
 
-# Import notebooks and data
-git clone https://github.com/mcapuccini/TheSparkBox.git $HOME/.TheSparkBox/data
-
 # Start TheSparkBox
 docker run \
   -v /var/run/docker.sock:/var/run/docker.sock \
